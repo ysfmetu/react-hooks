25fe7fba3d98cdca4ef2be2a8c2ee3d678f2d128
yusuf ülkü~ <ysf.metu@gmail.com>
2023-03-16 12:35:20 +0300

    todo list oluşturma ekranı

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,25 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-const TodoList=()=>{
-  const [todos,setTodos]=useState([
-    {text:"get up the bed",id:1},
-    {text:"go to work lab",id:2},
-    {text:"continue the work",id:3},
-    {text:"feed yourself",id:4},
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { text: "get up the bed", id: 1 },
+    { text: "go to work lab", id: 2 },
+    { text: "continue the work", id: 3 },
+
   ]);
-  
-    return(
-        <div>
-           
-        </div>
-    );
 
-}
+  return (
+  <div>
+    <ul>
+        {todos.map( todo =>{
+            return(
+                <li key={todo.id}>{todo.text}</li>
+            );
+        }
+
+        )}
+    </ul>
+  </div>
+  );
+};
 export default TodoList;
