b34d84eb1883a569f08a44209728091a63541f0e
yusuf ülkü~ <ysf.metu@gmail.com>
2023-03-16 12:10:53 +0300

    first initiaze TodoList oluşturma

---
+++ src/components/TodoList.js
@@ -0,0 +1,18 @@
+import React,{useState} from "react";
+
+const TodoList=()=>{
+  const [todos,setTodos]=useState([
+    {text:"get up the bed",id:1},
+    {text:"go to work lab",id:2},
+    {text:"continue the work",id:3},
+    {text:"feed yourself",id:4},
+  ]);
+  
+    return(
+        <div>
+           
+        </div>
+    );
+
+}
+export default TodoList;
